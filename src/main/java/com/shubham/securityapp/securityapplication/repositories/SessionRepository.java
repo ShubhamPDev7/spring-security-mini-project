85041dad4e7129c8c041ef3b50bc2dceb0030c10
Shubham Pawar <shubhampawar5929@gmail.com>
2026-04-07 17:48:11 +0530

    Added example config files and secured sensitive data

---
+++ src/main/java/com/shubham/securityapp/securityapplication/repositories/SessionRepository.java
@@ -0,0 +1,14 @@
+package com.shubham.securityapp.securityapplication.repositories;
+
+import com.shubham.securityapp.securityapplication.entities.Session;
+import com.shubham.securityapp.securityapplication.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SessionRepository extends JpaRepository<Session,Long> {
+    List<Session> findByUser(User user);
+
+    Optional<Session> findByRefreshToken(String refreshToken);
+}
