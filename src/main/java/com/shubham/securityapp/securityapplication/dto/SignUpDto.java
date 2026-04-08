852f373a216c4567e671f1ae1a7464c774434640
Shubham Pawar <shubhampawar5929@gmail.com>
2026-04-08 17:35:41 +0530

    feat: add Role based Authorization in Spring Security

--- src/main/java/com/shubham/securityapp/securityapplication/dto/SignUpDto.java
+++ src/main/java/com/shubham/securityapp/securityapplication/dto/SignUpDto.java
@@ -1,10 +1,14 @@
 package com.shubham.securityapp.securityapplication.dto;
 
+import com.shubham.securityapp.securityapplication.entities.enums.Role;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class SignUpDto {
     private String email;
     private String password;
     private String name;
+    private Set<Role> roles;
 }
---
+++ src/main/java/com/shubham/securityapp/securityapplication/entities/enums/Role.java
@@ -0,0 +1,9 @@
+package com.shubham.securityapp.securityapplication.entities.enums;
+
+
+public enum Role {
+
+    USER,
+    CREATOR,
+    ADMIN
+}
